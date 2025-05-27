a85db5ed4be98058f0f6ef81560419b7432bca90
DereckAn <dereckangeles15071998@gmail.com>
2025-05-26 22:53:35 -0600

    Making the tile responsive

--- components/hero/ChanginColorText.tsx
+++ components/hero/ChanginColorText.tsx
@@ -1,18 +1,20 @@
 interface ChanginColorTextProps {
   text: string;
   className?: string;
+  fontSize?: string;
 }
 
 export const ChanginColorText = ({
   text,
   className,
+  fontSize = "clamp(3rem, 7vw, 15rem)", 
 }: ChanginColorTextProps) => {
   return (
     <span
       className={className}
       style={{
-        fontSize: "clamp(3rem, 7vw, 15rem)",
-        // whiteSpace: "nowrap",
+        fontSize: fontSize,
+        whiteSpace: "nowrap",
         animation: "colorTransition 2s ease-in-out 3s forwards",
       }}
     >
